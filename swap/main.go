86b0b728565c552772ac0f8dd660deef4126b419
frankie <frankie.li@kiwigame.com>
2025-11-25 11:52:39 +0800

    two finish

--- swap/main.go
+++ swap/main.go
@@ -2,9 +2,22 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
-func swap[T any](a, b T) {}
+func swap[T any](a, b T) {
+	va := reflect.ValueOf(a)
+	vb := reflect.ValueOf(b)
+
+	if va.Kind() != reflect.Ptr || vb.Kind() != reflect.Ptr {
+		panic("swap requires pointer")
+	}
+
+	tmp := reflect.New(va.Elem().Type())
+	tmp.Elem().Set(va.Elem())
+	va.Elem().Set(vb.Elem())
+	vb.Elem().Set(tmp.Elem())
+}
 
 func main() {
 	a := 10
